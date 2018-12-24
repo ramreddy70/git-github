d1ce68ca4a0bd8f6064e2411101f09a1130b8151
ramreddy70 <45449489+ramreddy70@users.noreply.github.com>
2018-12-24 07:29:21 +0530

    javatestfile.java
    cncknck

---
+++ javatestfile.java
@@ -0,0 +1,22 @@
+class A
+{ 
+public void display()
+System.out.println("This is display function");
+}
+}
+public class test
+{
+static void display()
+{
+System.out.println("display");
+}
+public static void main(string[] args)
+{
+int a=10;
+int b=a;
+A obj=new A();
+obj.display();
+display();
+system.out.println("Hello world ");
+}
+}
